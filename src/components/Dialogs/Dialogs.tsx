1676ca76fe40ee012d28387414bcdb2ff0788c24
Maksym <gurinmaxim11@gmail.com>
2022-04-06 10:47:54 +0300

    SM20

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import s from './Dialogs.module.css'
 
 
-export function Dialog(){
+export function Dialogs(){
     return (
         <div>
-            d
+            dialogs
         </div>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Navbar.module.css";
 
 export function Navbar() {
     return (
         <nav className={s.content}>
             <div className = {s.item}>
-                <a>Profile</a>
-            </div>
-            <div className = {`${s.item} ${s.active}`}>
-                <a>Messages</a>
+                <NavLink to={'/profile'} activeClassName={s.active}>Profile</NavLink>
             </div>
             <div className = {s.item}>
-                <a>News</a>
+                <NavLink to={'/dialogs'} activeClassName={s.active}>Messages</NavLink>
             </div>
             <div className = {s.item}>
-                <a>Music</a>
+                <NavLink to={'/news'} activeClassName={s.active}>News</NavLink>
             </div>
             <div className = {s.item}>
-                <a>Setings</a>
+                <NavLink to={'/music'} activeClassName={s.active}>Music</NavLink>
+            </div>
+            <div className = {s.item}>
+                <NavLink to={'/settings'} activeClassName={s.active}>Settings</NavLink>
             </div>
         </nav>
     );
